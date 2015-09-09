ff1bf6a4ad9e11f900fce68903097768115a0e7d
Andrea Stella <adr.stella@gmail.com>
2015-09-09 14:26:45 -0600

    Add files

---
+++ public/assets/script.js
@@ -0,0 +1,32 @@
+var app = angular.module('lucent', ['ngAnimate', 'ngRoute']);
+
+app.config(function($routeProvider) {
+	var routes = ['/', '/about', '/projects', '/contact', '/projects/:project'];
+
+	routes.map(addRoute);
+
+	function addRoute(route) {
+		var name = route.substring(1) || 'home';
+		if (name.indexOf(':') > -1) name = name.split(':')[1];
+
+		$routeProvider.when(route, {
+			templateUrl: 'partials/'+name+'.html',
+			controller: name
+		});
+	}
+});
+
+app.controller('main', function($scope, $rootScope) {
+	$scope.m = {isHome: false};
+	$rootScope.$on('$routeChangeSuccess', function(e, curr) {
+		$scope.m.isHome = (curr.templateUrl === 'partials/home.html');
+	})
+});
+
+app.controller('home', function($scope) {
+
+});
+
+app.controller('projects', function() {
+
+});
