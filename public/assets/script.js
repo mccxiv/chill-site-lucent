3911bffbeec220ef6f112c1402b4be635f185e01
Andrea Stella <adr.stella@gmail.com>
2015-09-11 17:35:01 -0600

    Save development snapshot

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,4 +1,4 @@
-var app = angular.module('lucent', ['ngAnimate', 'ngRoute']);
+var app = angular.module('lucent', ['ngAnimate', 'ngRoute', 'ngResource', 'ngSanitize']);
 
 app.config(function($routeProvider) {
 	var routes = ['/', '/about', '/projects', '/contact', '/projects/:project'];
@@ -20,15 +20,15 @@ app.controller('main', function($scope, $rootScope) {
 	$scope.m = {isHome: false};
 	$rootScope.$on('$routeChangeSuccess', function(e, curr) {
 		$scope.m.isHome = (curr.templateUrl === 'partials/home.html');
-	})
+	});
 });
 
 app.controller('home', function($scope) {
 
 });
 
-app.controller('projects', function() {
-
+app.controller('projects', function($scope, $resource) {
+	$scope.m.posts = $resource('../api/posts/', null, {get: {isArray: true}}).get();
 });
 
 app.filter('markdown', function() {
