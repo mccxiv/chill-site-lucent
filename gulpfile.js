ac978c93ad6a4969f060b33f3ae89be96c33eda0
Andrea Stella <adr.stella@gmail.com>
2015-10-02 09:18:33 -0600

    Add build process

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var fs =              require('fs');
+var del =             require('del');
+var gulp =            require('gulp');
+var useref =          require('gulp-useref');
+var uglify =          require('gulp-uglify');
+var gulpif =          require('gulp-if');
+var concat =          require('gulp-concat');
+var base64 =          require('gulp-css-base64');
+var minifyCss =       require('gulp-minify-css');
+var ngAnnotate =      require('gulp-ng-annotate');
+var runSequence =     require('run-sequence');
+
+gulp.task('default', ['make-dist']);
+
+gulp.task('make-dist', function(cb) {
+	runSequence('clean', 'copy-favicon', 'concat-minify-replace', cb);
+});
+
+gulp.task('clean', function() {
+	return del('dist');
+});
+
+gulp.task('copy-favicon', function() {
+	return gulp.src('src/favicon.png')
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('concat-minify-replace', function() {
+	var assets = useref.assets({noconcat: true});
+	return gulp.src('src/index.html')
+		.pipe(assets)
+		.pipe(gulpif('*.js', concat('site.min.js')))
+		.pipe(gulpif('*.js', ngAnnotate()))
+		.pipe(gulpif('*.js', uglify({output: {max_line_len: 200}})))
+		.pipe(gulpif('*.css', base64({maxWeightResource: Infinity})))
+		.pipe(gulpif('*.css', concat('site.min.css')))
+		.pipe(gulpif('*.css', minifyCss()))
+		.pipe(assets.restore())
+		.pipe(useref())
+		.pipe(gulp.dest('dist'));
+});
